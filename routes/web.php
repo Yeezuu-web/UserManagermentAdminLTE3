86d4b547c078d497ab3f240072513a721c9fa3c1
Piseth Chhun <smithiesbay@gmail.com>
2021-06-22 19:21:41 +0700

    Redirect route

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use App\Http\Controllers\Auth\ChangePasswordController;
 Route::redirect('/', '/login');
 Route::get('/home', function () {
     if (session('status')) {
-        return redirect()->route('admin.home')->with('status', session('status'));
+        return redirect()->route('admin.files.index')->with('status', session('status'));
     }
 
-    return redirect()->route('admin.home');
+    return redirect()->route('admin.files.index');
 });
 
 Auth::routes(['register' => false]);
